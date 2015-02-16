789590e10fb58217e3c00994c3e000a1e2e9ac76
How2dock <how2dock@gmail.com>
2015-02-16 10:53:20 +0100

    centos7 box

--- ch01/centos7/docker-bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo yum -y update
-sudo yum clean all
-sudo yum -y install epel-release
-sudo yum clean all
-sudo yum -y install docker-io
-sudo service docker start
-sudo gpasswd -a vagrant docker

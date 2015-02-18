dcdfc837a89278b6f0abe2ba06e189b44cf4ac03
How2dock <how2dock@gmail.com>
2015-02-18 19:50:25 +0100

    ECS convenience scripts

--- ch08/ecs/ecs-boto.py
+++ ch08/ecs/ecs-boto.py
@@ -15,4 +15,5 @@ echo ECS_CLUSTER=foobar >> /etc/ecs/ecs.config
 # Start instances with the testprofile
 
 c = boto.connect_ec2()
-c.run_instances('ami-34ddbe5c', key_name='ecs', instance_type='t2.micro', instance_profile_name='cookbook', user_data=base64.b64encode(userdata))
+#c.run_instances('ami-34ddbe5c', key_name='ecs', instance_type='t2.micro', instance_profile_name='cookbook', user_data=base64.b64encode(userdata))
+c.run_instances('ami-34ddbe5c', key_name='ecs', instance_type='t2.micro', instance_profile_name='cookbook')

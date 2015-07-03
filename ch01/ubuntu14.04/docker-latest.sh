29509b35424a61faabae8a48932eb21994a24b98
how2dock <how2dock@gmail.com>
2015-07-03 10:13:59 +0200

    change docker install script for latest version on ubuntu

--- ch01/ubuntu14.04/docker-latest.sh
+++ ch01/ubuntu14.04/docker-latest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+sudo curl -sSL https://get.docker.com | sudo sh
 sudo gpasswd -a vagrant docker
 sudo service docker restart

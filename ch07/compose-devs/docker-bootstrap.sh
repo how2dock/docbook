fcc3b091a49aa7a9c3006205bda55918a6565c9d
Trevor Hartman <trevorhartman@gmail.com>
2015-12-02 11:32:44 -0500

    Remove deprecated /ubuntu on get.docker.com installer script

--- ch07/compose-devs/docker-bootstrap.sh
+++ ch07/compose-devs/docker-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+sudo curl -sSL https://get.docker.com/ | sudo sh
 sudo gpasswd -a vagrant docker
 sudo service docker restart
 cd /usr/local/bin
--- ch07/compose-devs/docker-latest.sh
+++ ch07/compose-devs/docker-latest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+sudo curl -sSL https://get.docker.com/ | sudo sh
 sudo gpasswd -a vagrant docker
 sudo service docker restart
 curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose

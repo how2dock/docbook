19f73a2b0a0a5bbd65c76286ae7c05e6bf9d081b
Phil Mocek <phil@mocek.org>
2017-01-12 09:52:06 -0800

    Use Hashicorp releases service instead of Bintray

--- ch02/vagrantprovider/docker-bootstrap.sh
+++ ch02/vagrantprovider/docker-bootstrap.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get -y install docker.io
 sudo apt-get -y install wget
-sudo wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
+sudo wget https://releases.hashicorp.com/vagrant/1.7.2/vagrant_1.7.2_x86_64.deb
 sudo dpkg -i vagrant_1.7.2_x86_64.deb
 sudo gpasswd -a vagrant docker
 sudo service docker.io restart
